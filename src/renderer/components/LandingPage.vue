c8e3ef05879631168d122565ed5d8c40f2b9c2a4
Irko Palenius <ispal84@gmail.com>
2018-01-27 13:33:06 +0200

    Pumbed version

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Testing v0.0.14
+          Testing v0.0.15
         </span>
         <system-information></system-information>
       </div>
